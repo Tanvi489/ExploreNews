c263f48e83cc6dfd1b4f0e70e7ab1f41f73ac25b
Tanvi489 <tanviadevani.bscibm2024@iar.ac.in>
2026-07-25 16:14:42 +0530

    Updated RSS feeds

--- fetch_news.py
+++ fetch_news.py
@@ -6,7 +6,10 @@ gc = gspread.service_account(filename='google_creds.json')
 sh = gc.open("Explore_News_Beta_Staging").sheet1
 
 rss_urls = [
-    "https://rss.nytimes.com/services/xml/rss/nyt/Education.xml"
+    "https://rss.nytimes.com/services/xml/rss/nyt/Education.xml",
+    "https://www.the74million.org/feed/",
+    "https://www.highereddive.com/feeds/news/",
+    "https://www.k12dive.com/feeds/news/"
 ]
 
 existing_urls = sh.col_values(2)
